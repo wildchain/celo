18caef8929aa193b0ec9b47e2d4cd6d53cc62533
shubhamW1ldcha1n <79827911+shubhamW1ldcha1n@users.noreply.github.com>
2021-05-13 19:11:28 +0530

    Added for new helloworld

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+var HelloWorld = artifacts.require('HelloWorld')
 
-contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
-
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+module.exports = function (deployer) {
+  deployer.deploy(HelloWorld)
 }
